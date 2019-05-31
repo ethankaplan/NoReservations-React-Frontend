aac2bdf291abfad6554d7654875945e1675e7e28
Ethan <N/A>
2019-05-30 17:21:16 -0700

    Cannot access My Account if not logged in

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import RestAndRes from "./component/ViewAll/RestAndRes";
 import { Grid, Image } from "semantic-ui-react";
 import * as routes from "./constants/routes";
 import UserProfile from './component/UserProfile/UserProfile'
-
+import IncomingUser from './component/IncomingUser/SignInAndReg'
 class App extends Component {
   state = {
     currentUser: null
@@ -28,7 +28,11 @@ class App extends Component {
         />{" "}
         <Switch>
           <Route exact path={routes.HOME} render={() => <div> HOME </div>} />
-          <Route exact path={routes.USER} render={() => <UserProfile user={this.state.currentUser}/>} />
+          <Route exact path={routes.USER} 
+          render={this.props.currentUser
+            ? () =><UserProfile user={this.state.currentUser}/>:
+            ()=><IncomingUser doSetCurrentUser={this.doSetCurrentUser}/>
+          } />
           <Route exact path={routes.RESTR} render={() => <RestAndRes currentUser={this.state.currentUser} />} />{" "}
           <Route render={() => <div> NOT FOUND </div>} />
         </Switch>
